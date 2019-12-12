585d59657fc9e524f7a0988cacb09a7fbef63b84
Adrian Lopez Gonzalez <adlo@uoc.edu>
2019-12-12 15:23:40 +0100

    feat: change views and add a nicier style to the systema dministration

--- myhealthee/src/systemadministration/controller/UserController.java
+++ myhealthee/src/systemadministration/controller/UserController.java
@@ -1,6 +1,5 @@
 package systemadministration.controller;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import javax.ejb.EJB;
@@ -8,7 +7,10 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.inject.Named;
 
+import entity.Doctor;
 import entity.User;
+import entity.imp.Administrator;
+import entity.imp.Patient;
 import systemadministration.dao.SystemAdministrationFacadeRemote;
 import utils.SessionUtils;
 
@@ -23,11 +25,16 @@ public class UserController implements Serializable {
 	/* Fields */
 	private String id;
 	private String password;
+	private boolean rememberMe;
+	private String resetEmail;
+	
+	// New user
+	private Patient newPatient;
+	private Doctor newDoctor;
+	private Administrator newAdministrator;
 
 	/**
 	 * Login Action.
-	 * 
-	 * @throws IOException
 	 */
 	public String login() {
 		User u = ejb.login(id, password);
@@ -51,10 +58,35 @@ public class UserController implements Serializable {
 	}
 
 	/**
-	 * RegisterAdministrator Action.
+	 * Reset Password Action.
 	 */
-	public void registerAdministrator() {
+	public String resetPassword() {
 		// TODO
+		return null;
+	}
+	
+	/**
+	 * Register patient Action.
+	 */
+	public String registerPatient() {
+		// TODO
+		return null;
+	}
+	
+	/**
+	 * Register Doctor Action.
+	 */
+	public String registerDoctor() {
+		// TODO
+		return null;
+	}
+	
+	/**
+	 * Register Administrator Action.
+	 */
+	public String registerAdministrator() {
+		// TODO
+		return null;
 	}
 
 	// Getters & Setters
@@ -72,5 +104,45 @@ public class UserController implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public boolean isRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
+
+	public String getResetEmail() {
+		return resetEmail;
+	}
+
+	public void setResetEmail(String resetEmail) {
+		this.resetEmail = resetEmail;
+	}
+
+	public Patient getNewPatient() {
+		return newPatient;
+	}
+
+	public void setNewPatient(Patient newPatient) {
+		this.newPatient = newPatient;
+	}
+
+	public Doctor getNewDoctor() {
+		return newDoctor;
+	}
+
+	public void setNewDoctor(Doctor newDoctor) {
+		this.newDoctor = newDoctor;
+	}
+
+	public Administrator getNewAdministrator() {
+		return newAdministrator;
+	}
+
+	public void setNewAdministrator(Administrator newAdministrator) {
+		this.newAdministrator = newAdministrator;
 	}
 }
