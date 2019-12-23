d37de632b9acfbfaba06c10e7765f04e0a987d00
apeleteiro <apeleteiro@uoc.edu>
2019-12-23 19:07:49 +0100

    Update ProfileFacade

--- myhealthee/src/components/profile/dao/ProfileFacade.java
+++ myhealthee/src/components/profile/dao/ProfileFacade.java
@@ -4,12 +4,12 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import entity.User;
+import entity.imp.*;
 import org.jboss.resteasy.logging.Logger;
 import org.jboss.resteasy.logging.impl.Log4jLogger;
 
-import entity.imp.FamilyDoctor;
-import entity.imp.MedicalSpeciality;
-import entity.imp.PrimaryHealthCareCenter;
+import utils.QueryNames;
 
 /**
  * Profile EJB.
@@ -25,42 +25,127 @@ public class ProfileFacade implements ProfileFacadeRemote {
 	private EntityManager em;
 
 	@Override
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public boolean changeFamilyDoctor(String id, FamilyDoctor newDoctor) {
+		try {
+			Patient patient = (Patient) this.getUser(id);
+			if (patient != null) {
+				patient.setFamilyDoctor(newDoctor);
+				em.merge(patient);
+				em.flush();
+			}
+		} catch (PersistenceException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean registerPatient(String id, String nif, String name, String surnames, String password, String email) {
 		return false;
 	}
 
 	@Override
-	public boolean registerPatient(String id, String nif, String name, String surname, String password, String email) {
+	public boolean registerSpecialistDoctor(String id, String nif, String name, String surnames, String password, String email, MedicalSpeciality speciality) {
 		return false;
 	}
 
 	@Override
-	public boolean registerSpecialistDoctor(String id, String nif, String name, String surname, String password, String email, MedicalSpeciality speciality) {
+	public boolean registerFamilyDoctor(String id, String nif, String name, String surnames, String password, String email, PrimaryHealthCareCenter cap) {
 		return false;
 	}
 
 	@Override
-	public boolean registerFamilyDoctor(String id, String nif, String name, String surname, String password, String email, PrimaryHealthCareCenter cap) {
-		return false;
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public boolean updatePatientData(String id, String nif, String name, String surnames, String password, String email) {
+		try {
+			Patient patient = (Patient) this.getUser(id);
+			if (patient != null) {
+				patient.setNif(nif);
+				patient.setName(name);
+				patient.setSurnames(surnames);
+				patient.setPassword(password);
+				patient.setEmail(email);
+				em.merge(patient);
+				em.flush();
+			}
+		} catch (PersistenceException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		return true;
 	}
 
 	@Override
-	public boolean updatePatientData(String id, String nif, String name, String surname, String password, String email) {
-		return false;
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public boolean updateSpecialistDoctorData(String id, String nif, String name, String surnames, String password, String email, MedicalSpeciality speciality) {
+		try {
+			SpecialistDoctor specialistDoctor = (SpecialistDoctor) this.getUser(id);
+			if (specialistDoctor != null) {
+				specialistDoctor.setNif(nif);
+				specialistDoctor.setName(name);
+				specialistDoctor.setSurnames(surnames);
+				specialistDoctor.setPassword(password);
+				specialistDoctor.setEmail(email);
+				specialistDoctor.setMedicalSpeciality(speciality);
+				em.merge(patient);
+				em.flush();
+			}
+		} catch (PersistenceException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		return true;
 	}
 
 	@Override
-	public boolean updateSpecialistDoctorData(String id, String nif, String name, String surname, String password, String email, MedicalSpeciality speciality) {
-		return false;
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public boolean updateFamilyDoctorData(String id, String nif, String name, String surnames, String password, String email, PrimaryHealthCareCenter cap) {
+		try {
+			FamilyDoctor familyDoctor = (FamilyDoctor) this.getUser(id);
+			if (familyDoctor != null) {
+				familyDoctor.setNif(nif);
+				familyDoctor.setName(name);
+				familyDoctor.setSurnames(surnames);
+				familyDoctor.setPassword(password);
+				familyDoctor.setEmail(email);
+				familyDoctor.setPrimaryHealthcareCenter(cap);
+				em.merge(patient);
+				em.flush();
+			}
+		} catch (PersistenceException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		return true;
 	}
 
 	@Override
-	public boolean updateFamilyDoctorData(String id, String nif, String name, String surname, String password, String email, PrimaryHealthCareCenter cap) {
-		return false;
-	}
-
-	@Override
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public boolean changePrimaryHealthCareCenter(String id, PrimaryHealthCareCenter newCenter) {
-		return false;
+		try {
+			FamilyDoctor familyDoctor = (FamilyDoctor) this.getUser(id);
+			if (familyDoctor != null) {
+				familyDoctor.setPrimaryHealthcareCenter(newCenter);
+				em.merge(familyDoctor);
+				em.flush();
+			}
+		} catch (PersistenceException e) {
+			logger.error(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public User getUser(String id) {
+		try {
+			return (User) em.createNamedQuery(QueryNames.GET_USER).setParameter("email", id)
+					.setParameter("username", id).getSingleResult();
+		} catch (NoResultException e) {
+			logger.error(e.getMessage());
+		}
+		return null;
 	}
 }
