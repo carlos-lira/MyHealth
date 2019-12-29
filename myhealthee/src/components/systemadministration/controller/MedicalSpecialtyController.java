78d9f330bc24d02185dcb2da441655e5b02b32c2
Adrian Lopez Gonzalez <adlo@uoc.edu>
2019-12-29 20:06:30 +0100

    feat: add sweetalert library to the proyect

--- myhealthee/src/components/systemadministration/controller/MedicalSpecialtyController.java
+++ myhealthee/src/components/systemadministration/controller/MedicalSpecialtyController.java
@@ -1,12 +1,15 @@
 package components.systemadministration.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import components.systemadministration.dao.SystemAdministrationFacadeRemote;
+import entity.imp.MedicalSpeciality;
 
 /**
  * Medical specialty bean
@@ -20,7 +23,53 @@ public class MedicalSpecialtyController implements Serializable {
 
 	@EJB
 	private SystemAdministrationFacadeRemote ejb;
-	
+
 	/* Fields */
+	private MedicalSpeciality medicalSpeciality;
+	private List<MedicalSpeciality> listMedicalSpecialty;
+
+	@PostConstruct
+	public void init() {
+		this.medicalSpeciality = new MedicalSpeciality();
+		this.listMedicalSpecialities();
+	}
+
+	// ACTIONS
+	public void addMedicalSpeciality() {
+		String name = this.medicalSpeciality.getName();
+		String description = this.medicalSpeciality.getDescription();
+		ejb.addMedicalSpecialty(name, description);
+		this.listMedicalSpecialities(); // Not optimal the ejb should return the entity added.
+	}
 	
+	public String showMedicalSpeciality() {
+		// TODO
+		return null;
+	}
+
+	public String updateMedicalSpeciality() {
+		// TODO
+		return null;
+	}
+
+	public String deleteMedicalSpeciality(final String name) {
+		ejb.deleteMedicalSpecialty(name);
+		this.listMedicalSpecialities();
+		return null;
+	}
+
+	// PRIVATE METHODS
+	private void listMedicalSpecialities() {
+		this.listMedicalSpecialty = (List<MedicalSpeciality>) ejb.listAllMedicalSpecialities();
+	}
+
+	// Getters & Setters
+	public MedicalSpeciality getMedicalSpeciality() {
+		return medicalSpeciality;
+	}
+
+	public List<MedicalSpeciality> getListMedicalSpecialty() {
+		return listMedicalSpecialty;
+	}
+
 }
